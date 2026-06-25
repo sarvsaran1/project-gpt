34de67c4cdcad1f9a4fed7b52c5fea10a97d0642
sarvsaran1 <sarvsaran1@gmail.com>
2026-06-25 11:37:15 +0530

    Add Socket.IO chat and AI integration

--- src/sockets/socket.server.js
+++ src/sockets/socket.server.js
@@ -1,12 +1,52 @@
 const {Server} = require("socket.io")
+const cookie = require("cookie")
+const jwt = require("jsonwebtoken")
+const userModel = require("../models/user.model")
+const aiService = require("../services/ai.service")
 
 function initSocketServer(httpServer){
 
     const io = new Server(httpServer,{})
 
-    io.on("connection", (socket) => {
-        console.log("New Socket Connection:", socket.id)
+    io.use(async(socket, next) => {
+        const Cookies = cookie.parse(socket.handshake.headers?.cookie || "")
+        if (!Cookies.token) {
+            next(new Error("Authentication error: No token provided"))
+        }
+
+
+
+        try {
+            const decoded = jwt.verify(Cookies.token, process.env.JWT_SECRET);
+            const user = await userModel.findById(decoded.id);
+            socket.user = user;
+            next()
+         
+        }  catch (err) {
+            
+            next(new Error("Authentication error: Invalid token"))
+        }  
+
     })
+
+
+io.on("connection",(socket) =>{
+
+    socket.on("ai-message", async (messagePayload) =>{
+
+        console.log(messagePayload)
+
+        const response = await aiService.generateResponse(messagePayload.content)
+
+        socket.emit('ai-response', {
+            content: response,
+            chat: messagePayload.chat
+        })
+
+    })
+   
+})
+
 }
 
 module.exports =  initSocketServer
